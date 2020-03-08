f6321516bd472f7c1a0110d758c88c74bf0161d6
Megan Ja <megan.ja@hotmail.com>
2020-03-08 10:45:21 -0500

    one collection view working (clubs in)

---
+++ clubHub/CollectionViewCellClubsIn.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCellClubsIn.swift
+//  clubHub
+//
+//  Created by c1843 on 3/8/20.
+//  Copyright © 2020 c1843. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellClubsIn: UICollectionViewCell {
+    @IBOutlet weak var clubName: UILabel!
+    
+    @IBOutlet weak var clubLogo: UIImageView!
+}
---
+++ clubHub/ViewControllerProfile.swift
@@ -0,0 +1,50 @@
+//
+//  ViewControllerProfile.swift
+//  clubHub
+//
+//  Created by c1843 on 3/8/20.
+//  Copyright © 2020 c1843. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerProfile: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionClubsIn: UICollectionView!
+    //@IBOutlet weak var collectionWishlist: UICollectionView!
+    
+   let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    var items2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+
+
+    // MARK: - UICollectionViewDataSource protocol
+
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionViewCellClubsIn
+
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.clubName.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+
+        return cell
+    }
+
+    // MARK: - UICollectionViewDelegate protocol
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+}
+
+
