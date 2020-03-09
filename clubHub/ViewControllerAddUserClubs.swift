8d6eb03e186862df5bfbb08bf1a26fa6ed44b0f5
Megan Ja <megan.ja@hotmail.com>
2020-03-08 20:43:42 -0500

    remembers what clubs you are part of in teh addUserClubs view controller

--- clubHub/ViewControllerAddUserClubs.swift
+++ clubHub/ViewControllerAddUserClubs.swift
@@ -15,6 +15,10 @@ class ViewControllerAddUserClubs: UIViewController, UICollectionViewDataSource, 
     let db = Firestore.firestore()
     @IBOutlet weak var allClubsCollection: UICollectionView!
     
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = [String]()
+    var selectedItems = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         db.collection("clubs").getDocuments(){ (querySnapshot, err) in
@@ -22,16 +26,47 @@ class ViewControllerAddUserClubs: UIViewController, UICollectionViewDataSource, 
                 let temp = "\(String(describing: document.get("name")!))"
                 print(temp)
                 self.items.append(temp)
+                self.selectedItems.append("0")
             }
-            DispatchQueue.main.async {
-                self.allClubsCollection.reloadData()
+            print("selected Items1")
+            print(self.selectedItems)
+            let userRef = self.db.collection("users").document(uid)
+            userRef.getDocument { (document, error) in
+                let tempList = document?.data()!["myClubs"]! as![Any]
+                print("tempList")
+                print(tempList)
+                
+                for i in 0..<tempList.count{
+                    print("went in for")
+                    print(self.items.count)
+                    for j in 0..<self.items.count{
+                        print()
+                        print(self.items[j])
+                        print((tempList[i] as! String))
+                        print()
+                        if self.items[j] == (tempList[i] as! String){
+                            self.selectedItems[j] = ("1")
+                        }else{
+                            self.selectedItems[j] = ("0")
+                        }
+                        print("selectedItems2")
+                        print(self.selectedItems)
+                    }
+                    
+                }
+                print("selectedItems3")
+                print(self.selectedItems)
+                
+                DispatchQueue.main.async {
+                    self.allClubsCollection.reloadData()
+                }
             }
         }
     }
     
     
-    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
-    var items = [String]()
+    
+    
     
     
     // MARK: - UICollectionViewDataSource protocol
@@ -49,7 +84,11 @@ class ViewControllerAddUserClubs: UIViewController, UICollectionViewDataSource, 
         
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
         cell.clubName.text = self.items[indexPath.item]
-        cell.backgroundColor = UIColor.white // make cell more visible in our example project
+        if self.selectedItems[indexPath.item] == "0" {
+            cell.backgroundColor = UIColor.white // make cell more visible in our example project
+        }else{
+            cell.backgroundColor = UIColor.yellow
+        }
         cell.layer.borderColor = UIColor(red: 0.83, green: 0.12, blue: 0.2, alpha: 1.0).cgColor
         cell.layer.borderWidth = 1
         
@@ -67,11 +106,13 @@ class ViewControllerAddUserClubs: UIViewController, UICollectionViewDataSource, 
             cell?.backgroundColor = UIColor.white // make cell more visible in our example project
             cell?.layer.borderColor = UIColor(red: 0.83, green: 0.12, blue: 0.2, alpha: 1.0).cgColor
             cell?.layer.borderWidth = 1
+            selectedItems[indexPath.item] = "0"
         }
         else{
             cell?.backgroundColor = UIColor.yellow
             cell?.layer.borderColor = UIColor(red: 0.83, green: 0.12, blue: 0.2, alpha: 1.0).cgColor
             cell?.layer.borderWidth = 1
+            selectedItems[indexPath.item] = "1"
         }
         
     }
@@ -86,13 +127,27 @@ class ViewControllerAddUserClubs: UIViewController, UICollectionViewDataSource, 
             var vc = segue.destination as! ViewControllerProfile
             vc.viewer = viewer
         }
-       
+        
         
     }
     var done = false
     @IBAction func doneBtn(_ sender: Any) {
         done = true
-        //add clubs the user selected
+        let userRef = db.collection("users").document(uid)
+        for i in 0..<selectedItems.count{
+            if selectedItems[i] == "1"{
+                userRef.updateData([
+                    "myClubs": FieldValue.arrayUnion([items[i]])
+                ])
+            }
+            if selectedItems[i] == "0"{
+                userRef.updateData([
+                    "myClubs": FieldValue.arrayRemove([items[i]])
+                ])
+            }
+        }
+        
     }
     
 }
+
