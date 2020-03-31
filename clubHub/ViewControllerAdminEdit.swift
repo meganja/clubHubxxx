31209ce5e6bf71f8bb6a4d61b7843eb5ada469bd
Megan Ja <megan.ja@hotmail.com>
2020-03-30 20:44:43 -0500

    admin edit is fixed

--- clubHub/ViewControllerAdminEdit.swift
+++ clubHub/ViewControllerAdminEdit.swift
@@ -41,20 +41,25 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
     
     @IBOutlet weak var roomNumTxtFld: UITextField!
     
-    @IBOutlet weak var sponsorNameTxtFld: UITextField!
-    
-    @IBOutlet weak var sponsorEmailTxtFld: UITextField!
-    
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var schoologyCode: UITextField!
-    @IBOutlet weak var meetingTimes: UITextField!
     @IBOutlet weak var AMPMSwitch: UISegmentedControl!
     @IBOutlet weak var moreInfo: UITextField!
+    @IBOutlet weak var meetingTimes: UITextView!
+    
+    @IBOutlet weak var name1: UITextField!
+    @IBOutlet weak var email1: UITextField!
+    @IBOutlet weak var name2: UITextField!
+    @IBOutlet weak var email2: UITextField!
+    @IBOutlet weak var name3: UITextField!
+    @IBOutlet weak var email3: UITextField!
     
     var timeOfDay = ""
     
     var categories = [String]()
     var selectedCategories = [String]()
+    var sponsorName = [String]()
+    var sponsorEmail = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +70,9 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
         
         self.genDescriptTxtFld.layer.borderColor = UIColor.lightGray.cgColor
         self.genDescriptTxtFld.layer.borderWidth = 1
+        
+        self.meetingTimes.layer.borderColor = UIColor.lightGray.cgColor
+        self.meetingTimes.layer.borderWidth = 1
         
         categories = ["Music/Arts", "Competitive", "Leadership", "Other", "Cultural/Community", "STEM", "Performance", "Intellectual", "Student Government", "School Pride", "Volunteer", "Business", "FCS"]
         for i in 0..<categories.count{
@@ -115,6 +123,9 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
                 else if String(describing: document.get("AM-PM")!) == "PM"{
                     self.commitSegControl.selectedSegmentIndex = 1
                 }
+                else if String(describing: document.get("AM-PM")!) == "Both"{
+                    self.commitSegControl.selectedSegmentIndex = 2
+                }
                 
                 let daysInfo = document.data()["days"]! as! [String]
                 print(daysInfo)
@@ -163,9 +174,22 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
                 self.roomNumTxtFld.text = String(describing: document.get("room")!)
                 
                 
-                let sponsorInfo = document.data()["sponsor"]! as! [Any]
-                self.sponsorNameTxtFld.text = "\(sponsorInfo[0])"
-                self.sponsorEmailTxtFld.text = "\(sponsorInfo[1])"
+                self.sponsorName = document.data()["sponsorsName"]! as! [String]
+                self.sponsorEmail = document.data()["sponsorsEmail"]! as! [String]
+                
+                self.name1.text = "\(self.sponsorName[0])"
+                self.email1.text = "\(self.sponsorEmail[0])"
+                if (self.sponsorName.count == 2){
+                    self.name2.text = "\(self.sponsorName[1])"
+                    self.email2.text = "\(self.sponsorEmail[1])"
+                }else if (self.sponsorName.count == 3){
+                    self.name2.text = "\(self.sponsorName[1])"
+                    self.email2.text = "\(self.sponsorEmail[1])"
+                    self.name3.text = "\(self.sponsorName[2])"
+                    self.email3.text = "\(self.sponsorEmail[2])"
+                }
+                
+                
                 
                 let firebaseCategory = document.data()["categories"]! as! [Any]
                 print()
@@ -181,7 +205,7 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
                         print("\(firebaseCategory[i])")
                         print(self.categories[j])
                         print("\(firebaseCategory[i])" == self.categories[j])
-        
+                        
                         if "\(firebaseCategory[i])" == self.categories[j]{
                             print("is i == 0? \(i == 0)")
                             print("what is i?\(i)")
@@ -384,6 +408,40 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
         }
     }
     
+    func checkSponsors() -> Bool{
+        if ((!name1.text!.isEmpty && !email1.text!.isEmpty) || (!name2.text!.isEmpty && !email2.text!.isEmpty) ||
+            (!name3.text!.isEmpty && !email3.text!.isEmpty)){
+            
+            if ((!name1.text!.isEmpty && !email1.text!.isEmpty)){
+                sponsorName.append("\(name1.text!)")
+                sponsorEmail.append("\(email1.text!)")
+            }
+            if ((!name2.text!.isEmpty && !email2.text!.isEmpty)){
+                sponsorName.append("\(name2.text!)")
+                sponsorEmail.append("\(email2.text!)")
+            }
+            if ((!name3.text!.isEmpty && !email3.text!.isEmpty)){
+                sponsorName.append("\(name3.text!)")
+                sponsorEmail.append("\(email3.text!)")
+            }
+            print("vaalid sponsor")
+            return true
+            
+        }
+        print("no sponsor")
+        return false
+    }
+    
+    func checkDaySelected() -> Bool{
+        if (mondaySwitch.isOn == true || tuesdaySwitch.isOn == true || wednesdaySwitch.isOn == true || thursdaySwitch.isOn == true || fridaySwitch.isOn == true){
+            print("no day selected")
+            
+            return true
+            
+        }
+        return false
+    }
+    
     @IBAction func doneClicked(_ sender: Any) {
         print(docID)
         checkSwitches()
@@ -403,131 +461,160 @@ class ViewControllerAdminEdit: UIViewController, UIImagePickerControllerDelegate
         else if AMPMSwitch.selectedSegmentIndex == 1{
             timeOfDay = "PM"
         }
+        else if AMPMSwitch.selectedSegmentIndex == 2{
+            timeOfDay = "Both"
+        }
         
-        let clubsRef = db.collection("clubs")
-        if (!nameTxtFld.text!.isEmpty &&
-            !genDescriptTxtFld.text.isEmpty &&
-            !roomNumTxtFld.text!.isEmpty &&
-            !sponsorNameTxtFld.text!.isEmpty &&
-            !sponsorEmailTxtFld.text!.isEmpty &&
-            !schoologyCode.text!.isEmpty &&
-            !meetingTimes.text!.isEmpty &&
-            !moreInfo.text!.isEmpty){
-            print("none empty")
-            print("\(nameTxtFld.text!)")
-            print(commit)
-            print("\(genDescriptTxtFld.text!)")
-            print("\(roomNumTxtFld.text!)")
-            print("\(sponsorNameTxtFld.text!)")
-            print("\(sponsorEmailTxtFld.text!)")
-            if checkMainCategory() == true{
-                clubsRef.document(docID).setData(
-                    ["name":"\(nameTxtFld.text!)",
-                        "days":days,
-                        "volunteer":volunteerOppSwitch.isOn,
-                        "commit":commit,
-                        "description":"\(genDescriptTxtFld.text!)",
-                        "room":"\(roomNumTxtFld.text!)",
-                        "sponsor":["\(sponsorNameTxtFld.text!)", "\(sponsorEmailTxtFld.text!)"],
-                        "schoology":"\(schoologyCode.text!)",
-                        "time":"\(meetingTimes.text!)",
-                        "AM-PM":timeOfDay,
-                        "link":"\(moreInfo.text!)"
-                ])
-                
-                let ref = Storage.storage().reference()
-                print("club: \(docID)")
-                let imgRef = ref.child("images/\(docID).png")
-                if let uploadData = self.editClubImgVw.image?.pngData(){
-                    imgRef.putData(uploadData, metadata: nil) { (metadata, error) in
-                        
-                        if error != nil{
-                            print(error)
-                        }
-                        
-                        self.performSegue(withIdentifier: "backToBrowsing", sender: "done")
-                    }
-                }
-                
-                //first, put category with identifier "2" first in array
-                var selectedOnes = [String]()
-                var selectedOnesOrdered = [String]()
-                
-                
-                for i in 0..<self.selectedCategories.count{
-                    
-                    //just for printing
-                    if(self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"){
-                        selectedOnes.append(self.categories[i])
-                    }
-                    
-                    
-                    if(self.selectedCategories[i] == "2"){
-                        var temp = self.selectedCategories[i]
-                        self.selectedCategories.remove(at: i)
-                        self.selectedCategories.insert(temp, at: 0)
-                        
-                        temp = self.categories[i]
-                        self.categories.remove(at: i)
-                        self.categories.insert(temp, at: 0)
-                    }
-                }
-                
-                
-                //just for printing re-ordered array
-                for i in 0..<self.selectedCategories.count{
-                    if(self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"){
-                        selectedOnesOrdered.append(self.categories[i])
-                    }
-                }
-                
-                print("selected categories: \(selectedOnes)")
-                print("reordered with important one first: \(selectedOnesOrdered)")
-                
-                
-                //then save array to club in firestore
-                let clubRef = self.db.collection("clubs").document(docID)
-                for i in 0..<self.selectedCategories.count{
-                    if self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"{
-                        clubRef.updateData([
-                            "categories": FieldValue.arrayUnion([self.categories[i]])
-                        ])
-                    }
-                    if self.selectedCategories[i] == "0"{
-                        clubRef.updateData([
-                            "categories": FieldValue.arrayRemove([self.categories[i]])
-                        ])
-                    }
-                }
-                
-                
-            }else{
-                if count2 == 0{
-                    let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Must pick a defining category", preferredStyle: .alert)
-                    
-                    // Create OK button with action handler
-                    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-                        print("Ok button tapped")
-                        
-                    })
-                    dialogMessage.addAction(ok)
-                    self.present(dialogMessage, animated: true, completion: nil)
-                }else{
-                    let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Too many defining categories picked.  Pick only one.", preferredStyle: .alert)
-                    
-                    // Create OK button with action handler
-                    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-                        print("Ok button tapped")
-                        
-                    })
-                    dialogMessage.addAction(ok)
-                    self.present(dialogMessage, animated: true, completion: nil)
+        var clubName = "\(nameTxtFld.text!)"
+        
+        
+        
+        
+        let characterset = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+        var noSpecialChar = false
+        if !nameTxtFld.text!.isEmpty{
+            var searchTerm = clubName[clubName.startIndex]
+            print("Search term = \(searchTerm)")
+            for i in (0..<characterset.count){
+                if "\(searchTerm)" == characterset[i]{
+                    noSpecialChar = true
                 }
             }
         }
-        else{
-            let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Not all fields are filled in", preferredStyle: .alert)
-            
+        print("noSpecialChar = \(noSpecialChar)")
+        
+        let clubsRef = db.collection("clubs")
+        if noSpecialChar{
+            if (!nameTxtFld.text!.isEmpty &&
+                !genDescriptTxtFld.text.isEmpty &&
+                !roomNumTxtFld.text!.isEmpty &&
+                !schoologyCode.text!.isEmpty &&
+                !meetingTimes.text!.isEmpty &&
+                !moreInfo.text!.isEmpty && checkSponsors() && checkDaySelected()){
+                print("none empty")
+                print("\(nameTxtFld.text!)")
+                print(commit)
+                print("\(genDescriptTxtFld.text!)")
+                print("\(roomNumTxtFld.text!)")
+                if checkMainCategory() == true{
+                    clubsRef.document(docID).setData(
+                        ["name":"\(nameTxtFld.text!)",
+                            "days":days,
+                            "volunteer":volunteerOppSwitch.isOn,
+                            "commit":commit,
+                            "description":"\(genDescriptTxtFld.text!)",
+                            "room":"\(roomNumTxtFld.text!)",
+                            "sponsorsName":sponsorName,
+                            "sponsorsEmail":sponsorEmail,
+                            "schoology":"\(schoologyCode.text!)",
+                            "time":"\(meetingTimes.text!)",
+                            "AM-PM":timeOfDay,
+                            "link":"\(moreInfo.text!)"
+                    ])
+                    
+                    let ref = Storage.storage().reference()
+                    print("club: \(docID)")
+                    let imgRef = ref.child("images/\(docID).png")
+                    if let uploadData = self.editClubImgVw.image?.pngData(){
+                        imgRef.putData(uploadData, metadata: nil) { (metadata, error) in
+                            
+                            if error != nil{
+                                print(error)
+                            }
+                            
+                            self.performSegue(withIdentifier: "backToBrowsing", sender: "done")
+                        }
+                    }
+                    
+                    //first, put category with identifier "2" first in array
+                    var selectedOnes = [String]()
+                    var selectedOnesOrdered = [String]()
+                    
+                    
+                    for i in 0..<self.selectedCategories.count{
+                        
+                        //just for printing
+                        if(self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"){
+                            selectedOnes.append(self.categories[i])
+                        }
+                        
+                        
+                        if(self.selectedCategories[i] == "2"){
+                            var temp = self.selectedCategories[i]
+                            self.selectedCategories.remove(at: i)
+                            self.selectedCategories.insert(temp, at: 0)
+                            
+                            temp = self.categories[i]
+                            self.categories.remove(at: i)
+                            self.categories.insert(temp, at: 0)
+                        }
+                    }
+                    
+                    
+                    //just for printing re-ordered array
+                    for i in 0..<self.selectedCategories.count{
+                        if(self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"){
+                            selectedOnesOrdered.append(self.categories[i])
+                        }
+                    }
+                    
+                    print("selected categories: \(selectedOnes)")
+                    print("reordered with important one first: \(selectedOnesOrdered)")
+                    
+                    
+                    //then save array to club in firestore
+                    let clubRef = self.db.collection("clubs").document(docID)
+                    for i in 0..<self.selectedCategories.count{
+                        if self.selectedCategories[i] == "1" || self.selectedCategories[i] == "2"{
+                            clubRef.updateData([
+                                "categories": FieldValue.arrayUnion([self.categories[i]])
+                            ])
+                        }
+                        if self.selectedCategories[i] == "0"{
+                            clubRef.updateData([
+                                "categories": FieldValue.arrayRemove([self.categories[i]])
+                            ])
+                        }
+                    }
+                    
+                    
+                }else{
+                    if count2 == 0{
+                        let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Must pick a defining category", preferredStyle: .alert)
+                        
+                        // Create OK button with action handler
+                        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                            print("Ok button tapped")
+                            
+                        })
+                        dialogMessage.addAction(ok)
+                        self.present(dialogMessage, animated: true, completion: nil)
+                    }else{
+                        let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Too many defining categories picked.  Pick only one.", preferredStyle: .alert)
+                        
+                        // Create OK button with action handler
+                        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                            print("Ok button tapped")
+                            
+                        })
+                        dialogMessage.addAction(ok)
+                        self.present(dialogMessage, animated: true, completion: nil)
+                    }
+                }
+            }
+            else{
+                let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Not all fields are filled in", preferredStyle: .alert)
+                
+                // Create OK button with action handler
+                let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                    print("Ok button tapped")
+                    
+                })
+                dialogMessage.addAction(ok)
+                self.present(dialogMessage, animated: true, completion: nil)
+            }
+        }else{
+            let dialogMessage = UIAlertController(title: "Uh-Oh", message: "Club Name can't begin with a space or special character", preferredStyle: .alert)
             // Create OK button with action handler
             let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
                 print("Ok button tapped")
