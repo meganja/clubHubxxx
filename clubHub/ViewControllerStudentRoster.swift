1a9e7f7ae38050fb50bff73ae25c880bde684697
Megan Ja <megan.ja@hotmail.com>
2020-04-20 17:13:43 -0500

    lists students

--- clubHub/ViewControllerStudentRoster.swift
+++ clubHub/ViewControllerStudentRoster.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewControllerStudentRoster: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     var viewer = ""
     var clubName = ""
+    
+    @IBOutlet weak var collectionStudents: UICollectionView!
+    let db = Firestore.firestore()
+    
+    
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var clubsRef = db.collection("clubs")
+        var sponsorsRef = db.collection("users")
         titleLabel.text = ("Students in \(clubName)")
+        
+        sponsorsRef.whereField("myClubs", arrayContains: self.clubName).getDocuments(){ (querySnapshot, error) in
+            print("got into first queury")
+            for document in querySnapshot!.documents{
+                if document.get("accountType") != nil{
+                    if String(describing: document.get("accountType")!) == "student"{
+                        self.items.append(String(describing: document.get("name")!))
+                    }
+                }
+                
+            }
+            DispatchQueue.main.async {
+                self.collectionStudents.reloadData()
+            }
+            
+        }
+        
+        
+        
         
     }
     
@@ -26,31 +55,40 @@ class ViewControllerStudentRoster: UIViewController, UICollectionViewDataSource,
     }
     
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
-
-
+    var items = [String]()
+    
+    
     // MARK: - UICollectionViewDataSource protocol
-
+    
     // tell the collection view how many cells to make
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
+        if self.items.count == 0{
+            return 1
+        }else{
+            return self.items.count
+        }
     }
-
+    
     // make a cell for each cell index path
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
+        
         // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionViewCellStudents
-
-        cell.crownBtn.tag = indexPath.item
-        cell.crownBtn.addTarget(self, action: #selector(crown(_:)), for: .touchUpInside)
         
-        cell.studentName.text = self.items[indexPath.item]
+        if self.items.count > 0{
+            
+            cell.backgroundColor = UIColor.white // make cell more visible in our example project
+            cell.layer.borderColor = UIColor(red: 0.83, green: 0.12, blue: 0.2, alpha: 1.0).cgColor
+            cell.layer.borderWidth = 1
+            cell.studentName.text = self.items[indexPath.item]
+            cell.crownBtn.isHidden = false
+            cell.crownBtn.tag = indexPath.item
+            cell.crownBtn.addTarget(self, action: #selector(crown(_:)), for: .touchUpInside)
+        }else{
+            cell.studentName.text = "No Enrollment"
+            cell.crownBtn.isHidden = true
+        }
         
-        cell.backgroundColor = UIColor.white // make cell more visible in our example project
-        cell.layer.borderColor = UIColor(red: 0.83, green: 0.12, blue: 0.2, alpha: 1.0).cgColor
-        cell.layer.borderWidth = 1
-
         return cell
     }
     
@@ -59,13 +97,13 @@ class ViewControllerStudentRoster: UIViewController, UICollectionViewDataSource,
         print("EDIT CLUB HAS BEEN CALLED, ONTO SEGUE")
         print("You selected cell #\(sender.tag)!")
     }
-
+    
     // MARK: - UICollectionViewDelegate protocol
-
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
     
-
+    
 }
